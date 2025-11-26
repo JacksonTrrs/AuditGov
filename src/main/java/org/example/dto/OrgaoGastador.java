e6177fd7e57b887ce6b3dc4f0df751ffb87b80e3
Sabrina Raiane L. Barbosa <sabrinaraiane2003@gmail.com>
2025-11-25 21:10:41 -0300

    move OrgaoGastador class to dto package

---
+++ src/main/java/org/example/dto/OrgaoGastador.java
@@ -0,0 +1,33 @@
+package org.example.dto;
+
+/**
+ * DTO (Data Transfer Object) para representar um órgão com seu valor total gasto.
+ * Usado para relatórios de maiores gastadores.
+ */
+public class OrgaoGastador {
+    private String nomeOrgao;
+    private double valorTotal;
+
+    public OrgaoGastador(String nomeOrgao, double valorTotal) {
+        this.nomeOrgao = nomeOrgao;
+        this.valorTotal = valorTotal;
+    }
+
+    public String getNomeOrgao() {
+        return nomeOrgao;
+    }
+
+    public void setNomeOrgao(String nomeOrgao) {
+        this.nomeOrgao = nomeOrgao;
+    }
+
+    public double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+}
+
+
